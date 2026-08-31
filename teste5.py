db11cc5759ad5143a3ef93a7918efb165671496c
Lucasfrancadd <lukasdt2006@gmail.com>
2026-08-31 12:15:30 -0300

    Refactor vehicle registration and consultation logic

--- teste5.py
+++ teste5.py
@@ -36,7 +36,8 @@ if menu == "Cadastrar veículo":
     )
     placa = st.text_input("Placa")
     diaria = st.number_input("Valor diária")
-    disponibilidade = st.selectbox("disponibilidade")
+    disponibilidade = st.selectbox("disponibilidade"
+                                  ["top", "center", "bottom"], index=2)
     
    
 
